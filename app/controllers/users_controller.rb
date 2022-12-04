2afecb233b56fa20c172153e05d0b0090aa4dd3f
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-12-04 19:31:59 -0400

    form_with buggy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,33 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
         @users = User.all
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to new_user_path(@user)
         else
             render :new, status: :unprocessable_entity
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(user_params)
+    
+        if @user.update(user_params)
+          redirect_to edit_user_path(@user)
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
+      
 
     private
     def user_params
